80a73a3c8226785f76589470a91b77dfa3c637d6
opes1 <opeoyinlola11@gmail.com>
2026-06-01 15:43:43 +0100

    Fix database config: use SQLite default, override with DATABASE_URL

--- unity_battalion/settings.py
+++ unity_battalion/settings.py
@@ -94,12 +94,8 @@ AUTH_USER_MODEL = 'accounts.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
